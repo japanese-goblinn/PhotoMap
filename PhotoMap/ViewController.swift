b35f1c360d2f3e562bde2366424563216cfd194c
japanese_joblinn <cool45akol@gmail.com>
2019-10-01 18:00:11 +0300

    Initial Commit

---
+++ PhotoMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMap
+//
+//  Created by Kiryl Harbachonak on 10/1/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
