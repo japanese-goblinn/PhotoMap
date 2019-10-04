b6d8981dcc614565695c27c8cae376d5a4fcee74
japanese_joblinn <cool45akol@gmail.com>
2019-10-04 13:36:07 +0300

    added CategoriesViewController

---
+++ PhotoMap/Controllers/CategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesViewController.swift
+//  PhotoMap
+//
+//  Created by Kiryl Harbachonak on 10/4/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
