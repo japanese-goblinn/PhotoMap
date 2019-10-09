a7ccc30cde50732a8d98f0e4cb638c79ba423f64
japanese_joblinn <cool45akol@gmail.com>
2019-10-09 14:27:00 +0300

    started to implementing custom callout view

---
+++ PhotoMap/Views/PhotoMarkCalloutView.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoMarkCalloutView.swift
+//  PhotoMap
+//
+//  Created by Kiryl Harbachonak on 10/9/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class PhotoMarkCalloutView: UIView {
+    
+    
+}
