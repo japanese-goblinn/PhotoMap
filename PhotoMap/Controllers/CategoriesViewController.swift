ce2601e54037e535e61f5a5d6866a11d519003ce
japanese_joblinn <cool45akol@gmail.com>
2019-10-09 14:23:28 +0300

    started to implement categories

--- PhotoMap/Controllers/CategoriesViewController.swift
+++ PhotoMap/Controllers/CategoriesViewController.swift
@@ -10,8 +10,43 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
 
+    @IBOutlet private weak var navigationBar: UINavigationBar!
+    @IBOutlet private weak var navigationBarItem: UINavigationItem!
+    
+    private lazy var tintColor = UIColor(hex: "#5ca1e1")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
         
     }
+        
+    private func setupNavigationBar() {
+        navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: tintColor
+        ]
+        navigationBarItem.title = "Categories"
+        navigationBarItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(donePressed(_:))
+        )
+        navigationBarItem.rightBarButtonItem?.tintColor = tintColor
+    }
+    
+    @objc private func donePressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
+
+extension CategoriesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
 }
