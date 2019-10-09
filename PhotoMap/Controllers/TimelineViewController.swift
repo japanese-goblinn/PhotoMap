be986cc487df2b3592c415d9097a99524fd34775
japanese_joblinn <cool45akol@gmail.com>
2019-10-09 14:20:59 +0300

    added timeline ViewController and .xib

---
+++ PhotoMap/Controllers/TimelineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewController.swift
+//  PhotoMap
+//
+//  Created by Kiryl Harbachonak on 10/9/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
