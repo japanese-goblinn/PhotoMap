66b20d23fbbc7586e18e48b1932b8e68fc10a088
japanese_joblinn <cool45akol@gmail.com>
2019-10-09 14:22:44 +0300

    added MoreViewController and .xib

---
+++ PhotoMap/Controllers/MoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoreViewController.swift
+//  PhotoMap
+//
+//  Created by Kiryl Harbachonak on 10/9/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
