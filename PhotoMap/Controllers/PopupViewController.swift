978ca939c38240afe2e03b0d5e82631480b48b71
japanese_goblinn <cool45akol@gmail.com>
2019-10-23 09:07:53 +0300

    trying to add interaction with keyboard

--- PhotoMap/Controllers/PopupViewController.swift
+++ PhotoMap/Controllers/PopupViewController.swift
@@ -12,8 +12,9 @@ class PopupViewController: UIViewController {
 
     var annotation: PhotoMarkAnnotation?
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var scrollView: UIScrollView!
+//    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var contentTextView: UITextView!
     
     @IBAction func doneButtonPressed(_ sender: UIButton) {
@@ -26,14 +27,64 @@ class PopupViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activateKeyboardHandler()
         setOutletsData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     private func setOutletsData() {
         if let annotation = annotation {
-            imageView.image = annotation.image
+//            imageView.image = annotation.image
             contentTextView.text = annotation.title
         }
     }
+}
+
+extension PopupViewController: UITextViewDelegate {
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           contentTextView.endEditing(true)
+           return false
+       }
+    
+    private func activateKeyboardHandler() {
+        contentTextView.delegate = self
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo ?? [:]
+        let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let contentInset = UIEdgeInsets(
+            top: 0, left: 0, bottom: keyboardFrame.height, right: 0
+        )
+        setContentInsets(contentInset)
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        setContentInsets(.zero)
+    }
+    
+    private func setContentInsets(_ insets: UIEdgeInsets) {
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+    }
 }
