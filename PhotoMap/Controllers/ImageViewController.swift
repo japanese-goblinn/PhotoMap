ec02a52d4b59bee53484d9cb1daad91493ef0ceb
japanese_goblinn <cool45akol@gmail.com>
2019-10-25 22:31:44 +0300

    started to implement full photo screen

---
+++ PhotoMap/Controllers/ImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewController.swift
+//  PhotoMap
+//
+//  Created by Kirill Gorbachyonok on 10/23/19.
+//  Copyright © 2019 Kiryl Harbachonak. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
